7bfbe9d80fc9b914ff14161078665fdf96ab9fa2
Carlos Menchu <carlosmenchu2@gmail.com>
2022-01-12 23:17:10 -0600

    Creacion de componente Home

---
+++ src/components/home/Home.js
@@ -0,0 +1,17 @@
+import { Container, Grid } from '@mui/material'
+import React from 'react'
+
+export const Home = () => {
+    return (
+       <Container maxWidth='lg'>
+            <Grid container>
+                <Grid xs={12} md={6}>
+
+                </Grid>
+                <Grid xs={12} md={6}>
+
+                </Grid>
+            </Grid>
+       </Container>
+    )
+}
